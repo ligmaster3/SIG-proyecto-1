f62055545df05eac4534355033ffdb3f2466ddf6
Enier <enigamer01@gmail.com>
2025-05-04 02:00:25 -0500

    Update index.php

--- views/prestamos/index.php
+++ views/prestamos/index.php
@@ -1,4 +1,4 @@
-<?php require_once '../views/layout/header.php'; ?>
+<?php require_once 'views\layout\header.php'; ?>
 
 <div class="container">
     <h1 class="my-4">Gestión de Préstamos</h1>
@@ -151,4 +151,4 @@
     </table>
 </div>
 
-<?php require_once '../views/layout/footer.php'; ?>
+<?php require_once 'views\layout\footer.php'; ?>
