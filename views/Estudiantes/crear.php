f1eef50868b2c7a1b90f11688b274c94f56a38d3
Enier <enigamer01@gmail.com>
2025-05-04 02:00:13 -0500

    Update crear.php

--- views/Estudiantes/crear.php
+++ views/Estudiantes/crear.php
@@ -1,4 +1,4 @@
-<?php require_once '../views/layout/header.php'; ?>
+<?php require_once 'views\layout\header.php'; ?>
 
 <div class="container">
     <h1 class="my-4">Registrar Nuevo Estudiante</h1>
@@ -139,4 +139,4 @@ document.querySelector('form').addEventListener('submit', function(e) {
 });
 </script>
 
-<?php require_once '../views/layout/footer.php'; ?>
+<?php require_once 'views\layout\footer.php'; ?>
